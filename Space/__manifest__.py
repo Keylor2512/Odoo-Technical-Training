2b70289fa6183bccc05cf817b08b4d274b2bee5d
keylor2512 <sistemas@corporacioncf.com>
2022-11-08 22:21:26 +0000

    Test

--- Space/__manifest__.py
+++ Space/__manifest__.py
@@ -1,20 +1,38 @@
-# -*- coding: utf-8 -*-
-
 {
-    'name':'Space',
-    'summary':'Training',
-    'description':'test',
-    'author':'Keylor',
-    'website':'https://www.odoo.com',
-    'category':'Training',
-    'version':'0.1',
-    'depends':['base'],
-    'data':[
-        
-        
+    'name': 'Space Mission',
+    'version': '0.6',
+    'summary': 'Odoo Application to control its Space Mission',
+    'description': """
+        Space Mission Application to Manage Space Control:
+            -
+            -
+            -
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['project','website'],
+    'data': [
+        'security/space_mission_groups.xml',
+        'security/space_mission_security.xml',
+        'security/ir.model.access.csv',
+        'views/space_mission_menuitems.xml',
+        'views/spaceship_views.xml',
+        'views/mission_views.xml',
+        'views/space_mission_web_templates.xml',
+        'report/mission_report_template.xml',
+        'views/project_views_inherit.xml',
+        'wizard/project_wizard.xml',
     ],
-    'demo':[
-        
-    ],
-    'license': 'OPL-1'
+    'demo': ['demo/spaceship_demo.xml',],
+    
+    'assets': {},
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
 }
+
