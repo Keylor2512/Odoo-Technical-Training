9553fbb8bfe69073f280f50c7c60f10724d4f865
Keylor2512 <odoo@ampd01.odoo.com>
2022-11-08 20:45:08 +0000

    Basic Module

---
+++ Space/__manifiest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Space',
+    'summary':'Training',
+    'description':'',
+    'author':'Keylor',
+    'website':'',
+    'category':'space',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+        
+        
+    ],
+    'demo':[
+        
+    ],
+}
